ec63ee744c3dc4213e94e74cdba95d11bd778f4b
DeeNicely <dbirky6520@gmail.com>
2019-05-02 08:58:19 -0400

    details and delete

---
+++ ElevenNote.Models/NoteEdit.cs
@@ -0,0 +1,33 @@
+﻿using ElevenNote.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteEdit
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public bool UpdateNote(NoteEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == model.NoteId && e.OwnerId == _userId);
+            
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
